59d4c767d4383b3c2bce64dcea5816aec8bb0c22
ShogoMark <trigmark87@gmail.com>
2023-10-11 01:02:13 +0100

    scripting

---
+++ Java_script_warmups/1-multi_languages.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const firstLine = "C is fun";
+const secLine = "Python is cool";
+const thirdLine = "JavaScript is amazing";
+
+console.log(firstLine);
+console.log(secLine);
+console.log(thirdLine);
